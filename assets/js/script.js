e7cede88858a68200356d0c41eb42f04bd4a8a16
German Renato Cobian <germancobian@hotmail.com>
2021-06-22 09:20:11 -0700

    Create litener for submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-//Library script
-alert("Hello world!")
 
 let myLibrary = [];
+let newBook;
+let newBook2;
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -12,4 +12,13 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+}
+newBook = new Book("Dawn", "Nietszche", "300", true)
+addBookToLibrary(newBook)
+newBook2 = new Book("Frankenstein", "M Shelley", "300", true)
+addBookToLibrary(newBook2)
+console.log("My Library", myLibrary)
+
+function onSubmit() {
+  alert("You clicked me")
 }
