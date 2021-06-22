0c836de6ebb3351f8460180f7ffb9830a3e49fc1
EroAuditore <alfa.fenrir@gmail.com>
2021-06-22 11:43:02 -0500

    OnSubmit funcion create a books and add to library

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
-
 let myLibrary = [];
 let newBook;
-let newBook2;
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -13,12 +11,23 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
-newBook = new Book("Dawn", "Nietszche", "300", true)
-addBookToLibrary(newBook)
-newBook2 = new Book("Frankenstein", "M Shelley", "300", true)
-addBookToLibrary(newBook2)
-console.log("My Library", myLibrary)
+// newBook = new Book("Dawn", "Nietszche", "300", true)
+// addBookToLibrary(newBook)
+// newBook2 = new Book("Frankenstein", "M Shelley", "300", true)
+// addBookToLibrary(newBook2)
+// console.log("My Library", myLibrary)
 
 function onSubmit() {
-  alert("You clicked me")
+  //Get the values from HTML from
+  titleValue = document.getElementById('titleInput').value;
+  authorValue = document.getElementById('authorInput').value;
+  pagesValue = document.getElementById('pagesInput').value;
+  readValue = document.getElementById('readInput').checked;
+
+  //Create the book
+  newBook = new Book(titleValue, authorValue, pagesValue, readValue)
+  //Add the book to the library
+  addBookToLibrary(newBook)
+  //Display the library
+  console.log("My Library", myLibrary)
 }
